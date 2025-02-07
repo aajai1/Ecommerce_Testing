cd86e3c3717de5a25e629249a68a0a634057f519
aajai1 <aajai6382@gmail.com>
2025-02-07 10:42:05 +0530

    with and Without Arrow Scroll Is Tested

---
+++ src/test/java/com/Test/WithArrow25.java
@@ -0,0 +1,32 @@
+package com.Test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.utils.Browser;
+import com.utils.LocatorsForElement;
+import com.utils.SourcesLink;
+
+public class WithArrow25 {
+	@Test
+	public void useArrow25() throws InterruptedException {
+		WebDriver driver= Browser.edgeDriver();
+		SourcesLink.link(driver);
+		WebElement logo = LocatorsForElement.XpathLocator(driver, "//*[@id=\"header\"]/div/div/div/div[1]/div/a/img");
+		Assert.assertTrue(logo.isDisplayed(), "logo is not Displayed");
+		JavascriptExecutor js =(JavascriptExecutor)driver;
+		js.executeScript("window.scrollTo(0,8285);");
+		Thread.sleep(3000);
+		js.executeScript("window.scrollTo(0,0);");
+		
+		WebElement title = LocatorsForElement.XpathLocator(driver, "//*[@id=\"scrollUp\"]/i");
+		Assert.assertTrue(title.isDisplayed(), "Full-Fledged Message is not displayed");
+		driver.quit();
+	}
+
+}
