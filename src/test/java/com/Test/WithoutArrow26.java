b925ba1d19d361bbca25045b706766c5f74fa56b
aajai1 <aajai6382@gmail.com>
2025-02-10 04:40:04 +0530

    test case updated without arrow

--- src/test/java/com/Test/WithoutArrow26.java
+++ src/test/java/com/Test/WithoutArrow26.java
@@ -21,6 +21,7 @@ public class WithoutArrow26 {
 		Assert.assertTrue(logo.isDisplayed(), "logo is not Displayed");
 		JavascriptExecutor js =(JavascriptExecutor)driver;
 		js.executeScript("window.scrollTo(0,8285);");
+		Thread.sleep(3000);
 		js.executeScript("window.scrollTo(0,0);");
 		WebElement title = LocatorsForElement.XpathLocator(driver, "//*[@id=\"scrollUp\"]/i");
 		Assert.assertTrue(title.isDisplayed(), "Full-Fledged Message is not displayed");
